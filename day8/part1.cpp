07cc1e691393d8f2c2816f6fc30aa9b891642a6a
henrytsyu <tsy22@ic.ac.uk>
2023-12-08 23:08:56 +0000

    feat: complete day8 part1

--- day8/part1.cpp
+++ day8/part1.cpp
@@ -1,15 +1,36 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 void solution(const vector<string> &lines) {
-  for (const string &line : lines) {
-    cout << line << endl;
+  string instr = lines[0];
+  map<string, pair<string, string>> map;
+  for (size_t i = 2; i < lines.size(); i++) {
+    string line = lines[i];
+    map[line.substr(0, 3)] = make_pair(line.substr(7, 3), line.substr(12, 3));
   }
+
+  int steps = 0;
+  string cur = "AAA";
+  while (cur != "ZZZ") {
+    auto it = map.find(cur);
+    if (it == map.end()) {
+      cout << "Key invalid" << endl;
+      exit(EXIT_FAILURE);
+    }
+    if (instr[steps % instr.size()] == 'L') {
+      cur = it->second.first;
+    } else {
+      cur = it->second.second;
+    }
+    steps++;
+  }
+  cout << steps << endl;
 }
 
 int main() {

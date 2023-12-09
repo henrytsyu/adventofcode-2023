dae19f79ab73d9192f2df716e2f8e94da32957d5
henrytsyu <tsy22@ic.ac.uk>
2023-12-09 00:04:18 +0000

    feat: complete day8 part2

--- day8/part2.cpp
+++ day8/part2.cpp
@@ -1,15 +1,63 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
+using ll = long long;
 using namespace std;
 
-void solution(const vector<string> &lines) {
-  for (const string &line : lines) {
-    cout << line << endl;
+ll gcd(ll a, ll b) {
+  if (b == 0) {
+    return a;
   }
+  return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return (a * b) / gcd(a, b); }
+
+void solution(const vector<string> &lines) {
+  string instr = lines[0];
+  map<string, pair<string, string>> map;
+  for (size_t i = 2; i < lines.size(); i++) {
+    string line = lines[i];
+    map[line.substr(0, 3)] = make_pair(line.substr(7, 3), line.substr(12, 3));
+  }
+
+  vector<string> curs;
+  for (const auto &pair : map) {
+    string f = pair.first;
+    if (f[2] == 'A') {
+      curs.push_back(f);
+    }
+  }
+
+  vector<int> stepsVec;
+  for (int i = 0; i < curs.size(); i++) {
+    int steps = 0;
+    string cur = curs[i];
+    while (cur[2] != 'Z') {
+      auto it = map.find(cur);
+      if (it == map.end()) {
+        cout << "Key invalid" << endl;
+        exit(EXIT_FAILURE);
+      }
+      if (instr[steps % instr.size()] == 'L') {
+        cur = it->second.first;
+      } else {
+        cur = it->second.second;
+      }
+      steps++;
+    }
+    stepsVec.push_back(steps);
+  }
+
+  ll mult = 1;
+  for (int steps : stepsVec) {
+    mult = lcm(mult, steps);
+  }
+  cout << endl << mult << endl;
 }
 
 int main() {

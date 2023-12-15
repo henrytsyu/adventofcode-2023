0ebe70f58211298717b69c6e7c9e8ade940b1136
henrytsyu <tsy22@ic.ac.uk>
2023-12-15 12:46:35 +0000

    feat: complete day9 part1

--- day9/part1.cpp
+++ day9/part1.cpp
@@ -2,14 +2,44 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 
+using ll = long long;
 using namespace std;
 
-void solution(const vector<string> &lines) {
-  for (const string &line : lines) {
-    cout << line << endl;
+bool all_zeros(const vector<int> &v) {
+  for (int i : v) {
+    if (i != 0) return false;
   }
+  return true;
+}
+
+void solution(const vector<string> &lines) {
+  ll sum = 0;
+  for (const string &line : lines) {
+    vector<vector<int>> sequences;
+    string s;
+    stringstream ss(line);
+    vector<int> v;
+    while (getline(ss, s, ' ')) {
+      v.push_back(stoi(s));
+    }
+    sequences.push_back(v);
+    while (!all_zeros(sequences.back())) {
+      vector<int> u;
+      vector<int> back = sequences.back();
+      for (int i = 1; i < back.size(); i++) {
+        u.push_back(back[i] - back[i - 1]);
+      }
+      sequences.push_back(u);
+    }
+    for (int i = sequences.size() - 2; i > 0; i--) {
+      sequences[i - 1].push_back(sequences[i - 1].back() + sequences[i].back());
+    }
+    sum += sequences[0].back();
+  }
+  cout << sum << endl;
 }
 
 int main() {

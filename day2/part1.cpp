03384c5191c0d7320669b2813a6aa281c6dcacbf
henrytsyu <tsy22@ic.ac.uk>
2023-12-16 08:41:32 +0000

    feat: complete day2 part1

--- day2/part1.cpp
+++ day2/part1.cpp
@@ -6,10 +6,35 @@
 
 using namespace std;
 
+#define R_MAX 12
+#define G_MAX 13
+#define B_MAX 14
+
+#define C_TO_MAX(c) (c == 'r' ? R_MAX : c == 'g' ? G_MAX : B_MAX)
+
 void solution(const vector<string> &lines) {
+  int sum = 0;
   for (const string &line : lines) {
-    cout << line << endl;
+    size_t cur = line.find(':');
+    int line_num = stoi(line.substr(5, cur - 5));
+    bool valid = true;
+    // skip blank
+    cur += 2;
+    while (cur != line.npos + 1) {
+      size_t int_pos = line.find(' ', cur);
+      int amount = stoi(line.substr(cur, int_pos - cur));
+      char colour = line.at(int_pos + 1);
+      if (amount > C_TO_MAX(colour)) {
+        valid = false;
+        break;
+      }
+      cur = line.find(' ', int_pos + 1) + 1;
+    }
+    if (valid) {
+      sum += line_num;
+    }
   }
+  cout << sum << endl;
 }
 
 int main() {
